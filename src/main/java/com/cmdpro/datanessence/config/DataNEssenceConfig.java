bd384a7e49eae219efbc0ead6e8cbb3928d0e1e0
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2024-05-29 20:21:54 -0400

    Update DataNEssenceConfig.java

--- src/main/java/com/cmdpro/datanessence/config/DataNEssenceConfig.java
+++ src/main/java/com/cmdpro/datanessence/config/DataNEssenceConfig.java
@@ -22,10 +22,10 @@ public class DataNEssenceConfig {
         essencePointTransferValue = buildInteger(builder, "essencePointTransfer", "all", 50, 1, 1000000, "How much should an essence point transfer? (Applies to all essence point types)");
         itemPointTransferValue = buildInteger(builder, "itemPointTransfer", "all", 4, 1, 64, "How much should an item point transfer?");
     }
-    private static ForgeConfigSpec.BooleanValue buildBoolean(ForgeConfigSpec.Builder builder, String name, String catagory, boolean defaultValue, String comment) {
+    private static ForgeConfigSpec.BooleanValue buildBoolean(ForgeConfigSpec.Builder builder, String name, String category, boolean defaultValue, String comment) {
         return builder.comment(comment).translation(name).define(name, defaultValue);
     }
-    private static ForgeConfigSpec.IntValue buildInteger(ForgeConfigSpec.Builder builder, String name, String catagory, int defaultValue, int min, int max, String comment) {
+    private static ForgeConfigSpec.IntValue buildInteger(ForgeConfigSpec.Builder builder, String name, String category, int defaultValue, int min, int max, String comment) {
         return builder.comment(comment).translation(name).defineInRange(name, defaultValue, min, max);
     }
     public static int fluidPointTransfer = 50;
