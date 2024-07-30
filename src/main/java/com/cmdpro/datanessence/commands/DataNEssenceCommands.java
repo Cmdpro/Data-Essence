12871bab64a025a60e89c9348dbca2aafb5b570a
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2024-07-30 14:28:39 -0400

    /unlock_all now exists

--- src/main/java/com/cmdpro/datanessence/commands/DataNEssenceCommands.java
+++ src/main/java/com/cmdpro/datanessence/commands/DataNEssenceCommands.java
@@ -63,6 +63,11 @@ public class DataNEssenceCommands {
                             return givedragonpart(command);
                         }))
                 )
+                .then(Commands.literal("unlock_all")
+                        .executes(command -> {
+                            return unlockall(command);
+                        })
+                )
         );
     }
     private static int checkoverlaps(CommandContext<CommandSourceStack> command) {
@@ -154,25 +159,17 @@ public class DataNEssenceCommands {
             }
         }
         return Command.SINGLE_SUCCESS;
-    }/*
-    private static int learnall(CommandContext<CommandSourceStack> command){
+    }
+    private static int unlockall(CommandContext<CommandSourceStack> command){
         if(command.getSource().getEntity() instanceof Player) {
             Player player = (Player) command.getSource().getEntity();
-            player.getCapability(PlayerModDataProvider.PLAYER_MODDATA).ifPresent(data -> {
-                data.setBookDiscoverProcess(2);
-                data.getUnlocked().clear();
-                for (Map.Entry<ResourceLocation, BookCategory> i : BookDataManager.get().getBook(new ResourceLocation(DataNEssence.MOD_ID, "datanessenceguide")).getCategories().entrySet()) {
-                    ArrayList<ResourceLocation> list = new ArrayList<>();
-                    for (Map.Entry<ResourceLocation, BookEntry> o : i.getValue().getEntries().entrySet()) {
-                        if (DataNEssenceUtil.getAnalyzeCondition(o.getValue().getCondition()) != null) {
-                            list.add(o.getKey());
-                        }
-                    }
-                    data.getUnlocked().put(i.getKey(), list);
-                }
-                BookUnlockStateManager.get().updateAndSyncFor((ServerPlayer)player);
-            });
+            for (Entry i : Entries.entries.values()) {
+                DataNEssenceUtil.DataTabletUtil.unlockEntry(player, i.id, false);
+            }
+            command.getSource().sendSuccess(() -> {
+                return Component.translatable("commands.datanessence.unlock_all");
+            }, true);
         }
         return Command.SINGLE_SUCCESS;
-    }*/
+    }
 }
