d7157c6949bac6615fb029a6b0920a0acb8a66da
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2024-08-09 22:39:01 -0400

    small fix

--- src/main/java/com/cmdpro/datanessence/screen/datatablet/pages/crafting/types/EntropicProcessorType.java
+++ src/main/java/com/cmdpro/datanessence/screen/datatablet/pages/crafting/types/EntropicProcessorType.java
@@ -1,6 +1,7 @@
 package com.cmdpro.datanessence.screen.datatablet.pages.crafting.types;
 
 import com.cmdpro.datanessence.api.ClientDataNEssenceUtil;
+import com.cmdpro.datanessence.recipe.EntropicProcessingRecipe;
 import com.cmdpro.datanessence.recipe.InfusionRecipe;
 import com.cmdpro.datanessence.registry.ItemRegistry;
 import com.cmdpro.datanessence.registry.RecipeRegistry;
@@ -16,18 +17,9 @@ import net.minecraft.world.item.crafting.Recipe;
 public class EntropicProcessorType extends CraftingType {
     @Override
     public void render(CraftingPage page, DataTabletScreen screen, GuiGraphics pGuiGraphics, int xOffset, int x, int yOffset, int y, Recipe recipe, int pMouseX, int pMouseY) {
-        if (recipe instanceof InfusionRecipe recipe2) {
+        if (recipe instanceof EntropicProcessingRecipe recipe2) {
             pGuiGraphics.blit(DataTabletScreen.TEXTURECRAFTING, xOffset + x, yOffset + y, 133, 136, 123, 60);
             page.renderIngredientWithTooltip(screen, pGuiGraphics, Ingredient.of(ItemRegistry.INFUSER_ITEM.get()), xOffset + x + 58, yOffset + y + 4, pMouseX, pMouseY);
-
-            ClientDataNEssenceUtil.EssenceBarRendering.drawEssenceBarTiny(pGuiGraphics, xOffset + x+5, yOffset + y+6, 0, recipe2.getEssenceCost(), 1000);
-
-            Component essence = ClientDataNEssenceUtil.EssenceBarRendering.getEssenceBarTooltipTiny(pMouseX, pMouseY, xOffset + x+5, yOffset + y+6, 0, recipe2.getEssenceCost());
-            if (essence != null) {
-                page.tooltipToShow.clear();
-                page.showTooltip = true;
-                page.tooltipToShow.add(essence.getVisualOrderText());
-            }
             
             page.renderItemWithTooltip(pGuiGraphics, recipe.getResultItem(RegistryAccess.EMPTY), xOffset + x + 82, yOffset + y + 22, pMouseX, pMouseY);
             page.renderIngredientWithTooltip(screen, pGuiGraphics, recipe2.getIngredients().get(0), xOffset + x + 38, yOffset + y + 22, pMouseX, pMouseY);
