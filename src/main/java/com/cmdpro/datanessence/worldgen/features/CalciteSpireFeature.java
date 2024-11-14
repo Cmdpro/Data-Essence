dd4f3fab2254c2f424ca4b3d5bf595d798bdc7d1
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2024-11-14 11:02:00 -0500

    spire fixes

--- src/main/java/com/cmdpro/datanessence/worldgen/features/CalciteSpireFeature.java
+++ src/main/java/com/cmdpro/datanessence/worldgen/features/CalciteSpireFeature.java
@@ -9,6 +9,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
@@ -22,46 +23,50 @@ public class CalciteSpireFeature extends Feature<NoneFeatureConfiguration> {
     @Override
     public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> pContext) {
         Vec3i offset = new Vec3i(8+pContext.random().nextIntBetweenInclusive(-4, 4), 0, 8+pContext.random().nextIntBetweenInclusive(-8, 8));
-        while (pContext.level().getBlockState(pContext.origin().offset(offset)).isAir()) {
-            offset = offset.offset(0, -1, 0);
-        }
-        float dirChange = pContext.random().nextBoolean() ? -(5+(pContext.random().nextFloat()*10)) : 5+(pContext.random().nextFloat()*10);
-        BlockPos origin = pContext.origin().offset(offset);
+        float height = 50*(pContext.random().nextFloat()+0.5f);
+        float spiralSize = (pContext.random().nextFloat()*0.25f)+0.75f;
+        float dirChange = pContext.random().nextBoolean() ? -(5+(pContext.random().nextFloat()*10)) : 5+(pContext.random().nextFloat()*5);
+        BlockPos origin = pContext.level().getHeightmapPos(Heightmap.Types.WORLD_SURFACE_WG, pContext.origin().offset(offset));
         float dir = pContext.random().nextFloat()*360;
-        for (int y = -5; y <= 50; y++) {
-            float range = (50-y)/10f;
+        for (int y = -5; y <= height; y++) {
+            float range = ((height-y)/10f);
             if (y < 0) {
                 range = 5-(y*-1);
             }
-            float size = 2;
+            float size = ((height-y)/15f);
             if (y < 0) {
                 size *= 1f-((y*-1f)/5f);
             }
-            if (y > 45) {
-                size *= 1f-((y-45)/5f);
-            }
-            for (int i = 0; i < 2; i++) {
-                float dirOffset = i*180;
-                BlockPos posCenter = origin.offset(new Vec3i((int) (Math.cos(Math.toRadians(dir+dirOffset)) * ((range * 2f) + 4)), y, (int) (Math.sin(Math.toRadians(dir+dirOffset)) * ((range * 2f) + 4))));
-                for (int x = (int)-Math.floor(size); x <= Math.ceil(size); x++) {
-                    for (int z = (int)-Math.floor(size); z <= Math.ceil(size); z++) {
-                        for (int y2 = (int)-Math.floor(size); y2 <= Math.ceil(size); y2++) {
-                            BlockPos pos = posCenter.offset(new Vec3i(x, y2, z));
-                            if (posCenter.getCenter().distanceTo(pos.getCenter()) <= size) {
-                                if (pContext.level().getBlockState(pos).canBeReplaced()) {
-                                    setBlock(pContext.level(), pos, BlockRegistry.SPIRE_GLASS.get().defaultBlockState());
+            size *= spiralSize;
+            float yDivideForSpeed = 20f;
+            int splits = (int)(3 * Math.clamp(y / yDivideForSpeed, 1, Float.MAX_VALUE));
+            for (int s = 0; s < splits; s++) {
+                for (int i = 0; i < 2; i++) {
+                    float dirOffset = i * 180;
+                    BlockPos posCenter = origin.offset(new Vec3i((int) (Math.cos(Math.toRadians(dir + dirOffset)) * ((range * 2f) + 4)), y, (int) (Math.sin(Math.toRadians(dir + dirOffset)) * ((range * 2f) + 4))));
+                    for (int x = (int) -Math.floor(size); x <= Math.ceil(size); x++) {
+                        for (int z = (int) -Math.floor(size); z <= Math.ceil(size); z++) {
+                            for (int y2 = (int) -Math.floor(size); y2 <= Math.ceil(size); y2++) {
+                                BlockPos pos = posCenter.offset(new Vec3i(x, y2, z));
+                                if (posCenter.getCenter().distanceTo(pos.getCenter()) <= size) {
+                                    if (pContext.level().getBlockState(pos).canBeReplaced()) {
+                                        setBlock(pContext.level(), pos, BlockRegistry.SPIRE_GLASS.get().defaultBlockState());
+                                    }
                                 }
                             }
                         }
                     }
                 }
+                dir += (dirChange * Math.clamp(y / yDivideForSpeed, 1, Float.MAX_VALUE)) / splits;
             }
-            dir += dirChange;
             for (int x = -10; x <= 10; x++) {
                 for (int z = -10; z <= 10; z++) {
                     BlockPos pos = origin.offset(new Vec3i(x, y, z));
-                    if (origin.offset(0, y, 0).getCenter().distanceTo(pos.getCenter()) <= range) {
-                        setBlock(pContext.level(), pos, Blocks.CALCITE.defaultBlockState());
+                    double dist = origin.offset(0, y, 0).getCenter().distanceTo(pos.getCenter());
+                    if (dist <= range) {
+                        if (dist <= 1 || (dist+1 <= range || pContext.random().nextIntBetweenInclusive(0, 4) != 0)) {
+                            setBlock(pContext.level(), pos, Blocks.CALCITE.defaultBlockState());
+                        }
                     }
                 }
             }
