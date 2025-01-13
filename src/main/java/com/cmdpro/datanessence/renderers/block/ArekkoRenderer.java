03e4c63eac106ca629ef60c183a9280cb14cf83d
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2025-01-13 04:39:09 -0500

    arekko

--- src/main/java/com/cmdpro/datanessence/renderers/block/ArekkoRenderer.java
+++ src/main/java/com/cmdpro/datanessence/renderers/block/ArekkoRenderer.java
@@ -5,19 +5,25 @@ import com.cmdpro.databank.model.DatabankModels;
 import com.cmdpro.databank.model.blockentity.DatabankBlockEntityModel;
 import com.cmdpro.databank.model.blockentity.DatabankBlockEntityRenderer;
 import com.cmdpro.datanessence.DataNEssence;
+import com.cmdpro.datanessence.block.processing.AutoFabricatorBlockEntity;
 import com.cmdpro.datanessence.block.technical.ArekkoBlockEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.animation.AnimationDefinition;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
 
 public class ArekkoRenderer extends DatabankBlockEntityRenderer<ArekkoBlockEntity> {
     public static final ModelLayerLocation modelLocation = new ModelLayerLocation(DataNEssence.locate("dead_makutuin"), "main");
 
     public ArekkoRenderer(BlockEntityRendererProvider.Context rendererProvider) {
-        super(new ArekkoRenderer.Model(rendererProvider.getModelSet().bakeLayer(modelLocation)));
+        super(new Model(rendererProvider.getModelSet().bakeLayer(modelLocation)));
     }
 
     @Override
@@ -47,8 +53,8 @@ public class ArekkoRenderer extends DatabankBlockEntityRenderer<ArekkoBlockEntit
         }
         public void setupAnim(ArekkoBlockEntity pEntity) {
             pEntity.animState.startIfStopped((int)getAgeInTicks());
-                this.animate(pEntity.animState, idle, 1.0f);
-            }
+            this.animate(pEntity.animState, idle, 1.0f);
+        }
 
         @Override
         public ModelPart root() {
