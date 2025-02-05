144e43dfc7eacdbbb2120e874261d13a3650110d
Gronglegrowth <fyredragonmagus@gmail.com>
2025-02-05 03:52:31 -0500

    Abandoned Factory

--- src/main/java/com/cmdpro/datanessence/block/LightFixture.java
+++ src/main/java/com/cmdpro/datanessence/block/LightFixture.java
@@ -2,7 +2,6 @@ package com.cmdpro.datanessence.block;
 
 import com.cmdpro.datanessence.api.block.RedirectorInteractable;
 import com.cmdpro.datanessence.particle.MoteParticleOptions;
-import com.cmdpro.datanessence.registry.ParticleRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
@@ -10,6 +9,8 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -47,7 +48,16 @@ public class LightFixture extends Block implements RedirectorInteractable {
         return true;
     }
 
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING).add(VARIANT);
+    public BlockState rotate(BlockState pState, Rotation pRotation) {
+        return pState.setValue(FACING, pRotation.rotate(pState.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return pState.rotate(pMirror.getRotation(pState.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING).add(VARIANT);
     }
 }
