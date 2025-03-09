8ce8313f6da50fb84f086845ca0811fd12a24044
Gronglegrowth <fyredragonmagus@gmail.com>
2025-03-09 10:05:19 -0400

    the Mekanismer Heresies gave chemical nodes a ticker, added proper recipes, Mekanism integration entry

--- src/main/java/com/cmdpro/datanessence/integration/mekanism/ChemicalNode.java
+++ src/main/java/com/cmdpro/datanessence/integration/mekanism/ChemicalNode.java
@@ -1,10 +1,14 @@
 package com.cmdpro.datanessence.integration.mekanism;
 
 import com.cmdpro.datanessence.api.node.block.BaseCapabilityPoint;
+import com.cmdpro.datanessence.block.transmission.ItemPointBlockEntity;
 import com.cmdpro.datanessence.registry.ItemRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,5 +30,14 @@ public class ChemicalNode extends BaseCapabilityPoint {
         if (!hasMekanism)
             return null;
         return new ChemicalNodeBlockEntity(pos, state);
+    }
+
+    @Override
+    public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        return (lvl, pos, st, blockEntity) -> {
+            if (blockEntity instanceof ChemicalNodeBlockEntity ent) {
+                ChemicalNodeBlockEntity.tick(lvl, pos, st, ent);
+            }
+        };
     }
 }
