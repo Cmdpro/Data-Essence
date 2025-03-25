6a5fc79f82ab85cea420314fb2d886f94b79be7f
Gronglegrowth <fyredragonmagus@gmail.com>
2025-03-25 19:06:04 -0400

    now it really begins

--- src/main/java/com/cmdpro/datanessence/registry/ItemRegistry.java
+++ src/main/java/com/cmdpro/datanessence/registry/ItemRegistry.java
@@ -70,6 +70,9 @@ public class ItemRegistry {
     public static final Supplier<Item> BONDING_POWDER = register("bonding_powder", () -> new Item(new Item.Properties()));
     public static final Supplier<Item> IRON_DRILL = register("iron_drill", () -> new Item(new Item.Properties()));
     public static final Supplier<Item> EXCITER = register("exciter", () -> new Item(new Item.Properties()));
+    public static final Supplier<Item> ECLIPTRUM_INGOT = register("ecliptrum_ingot", () -> new Item(new Item.Properties()));
+    public static final Supplier<Item> TRANSFORMATIVE_ROD = register("transformative_rod", () -> new Item(new Item.Properties()));
+    public static final Supplier<Item> REFLECTIVE_PANEL = register("reflective_panel", () -> new Item(new Item.Properties()));
 
     // Ore Processing Intermediates and Nuggets
     public static final Supplier<Item> COPPER_NUGGET = register("copper_nugget", () -> new Item(new Item.Properties()));
