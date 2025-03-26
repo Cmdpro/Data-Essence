6f30b41f9432ffa3db9130c32910134037abc29c
Gronglegrowth <fyredragonmagus@gmail.com>
2025-03-25 20:30:15 -0400

    overheatable interface

---
+++ src/main/java/com/cmdpro/datanessence/api/block/Overheatable.java
@@ -0,0 +1,22 @@
+package com.cmdpro.datanessence.api.block;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public interface Overheatable {
+    /**
+     * After failure, an overheatable block needs repair - this defines what with.
+     */
+    Ingredient canBeRepairedWith();
+
+    /**
+     * An overheatable block can sustain temperatures only up to this amount, after which it will fail.
+     */
+    int getMaxTemperature();
+
+    /**
+     * Upon going over temperature, an overheatable block will fail. This defines exactly how.
+     * @param block What to perform the failure on
+     */
+    void overheat(BlockEntity block);
+}
