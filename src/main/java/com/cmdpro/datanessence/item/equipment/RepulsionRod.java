f4ae7c0b4057089af18174655c90946606da330a
Gronglegrowth <fyredragonmagus@gmail.com>
2025-04-11 12:32:55 -0400

    Repulsion Rod fx

--- src/main/java/com/cmdpro/datanessence/item/equipment/RepulsionRod.java
+++ src/main/java/com/cmdpro/datanessence/item/equipment/RepulsionRod.java
@@ -1,5 +1,6 @@
 package com.cmdpro.datanessence.item.equipment;
 
+import com.cmdpro.databank.rendering.ColorUtil;
 import com.cmdpro.datanessence.DataNEssence;
 import com.cmdpro.datanessence.api.item.ItemEssenceContainer;
 import com.cmdpro.datanessence.client.particle.RhombusParticleOptions;
@@ -8,6 +9,8 @@ import com.cmdpro.datanessence.registry.DataComponentRegistry;
 import com.cmdpro.datanessence.registry.EssenceTypeRegistry;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
@@ -35,18 +38,33 @@ public class RepulsionRod extends Item {
 
         Level world = player.level();
 
+        world.playSound(null, target.getX(), target.getY(), target.getZ(), SoundEvents.ALLAY_THROW, SoundSource.PLAYERS, 1.0f, world.random.nextFloat());
         target.knockback(4.0d, player.getX() - target.getX(), player.getZ() - target.getZ());
         ItemEssenceContainer.removeEssence(stack, LUNAR, useCost);
 
+        // Particles
         Vec3 ang = player.getLookAngle();
         Vec3 pos = player.getEyePosition();
 
-        Color color = new Color(EssenceTypeRegistry.LUNAR_ESSENCE.get().color);
-
         for ( int i = 0; i < 12; i++ ) {
+            int rg = Math.clamp(world.random.nextInt(255), 50, 255);
+
+            var hsv = Color.RGBtoHSB(rg, rg, 0, null);
+            Color color = new Color(Color.HSBtoRGB(hsv[0], 0.5f, 1.0f));
+
+            float r1 = world.random.nextFloat() * 0.25f;
+            float r2 = world.random.nextFloat() * 0.45f + 1.0f;
+
+            double x = ( world.random.nextInt() % 2 == 0 ) ? pos.x+r1 : pos.x-r1;
+            double y = ( world.random.nextInt() % 2 == 0 ) ? pos.y+r1 : pos.y-r1;
+            double z = ( world.random.nextInt() % 2 == 0 ) ? pos.z+r1 : pos.z-r1;
+
+            double vx = ang.x*r2;
+            double vy = ang.y*r2+0.45;
+            double vz = ang.z*r2;
+
             var particle = ( world.random.nextInt() % 2 == 0 ) ? new RhombusParticleOptions(color, true) : new SmallCircleParticleOptions(color, true);
-            world.addParticle(particle, pos.x, pos.y-0.45, pos.z, ang.x*1.25, ang.y*1.25+0.45, ang.z*1.25);
-            // todo: random colors (desaturated yellows), random pos, random velocity
+            world.addParticle(particle, x, y-0.45, z, vx, vy, vz);
         }
 
         return InteractionResult.SUCCESS;
