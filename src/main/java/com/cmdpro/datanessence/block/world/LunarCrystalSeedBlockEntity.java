a53362cd314f8a953e28fbb14d39f5cb10c65687
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2025-04-27 23:30:06 -0400

    lunar crysral seed effects

--- src/main/java/com/cmdpro/datanessence/block/world/LunarCrystalSeedBlockEntity.java
+++ src/main/java/com/cmdpro/datanessence/block/world/LunarCrystalSeedBlockEntity.java
@@ -3,10 +3,7 @@ package com.cmdpro.datanessence.block.world;
 import com.cmdpro.datanessence.block.technical.StructureProtectorBlockEntity;
 import com.cmdpro.datanessence.client.particle.CircleParticleOptionsAdditive;
 import com.cmdpro.datanessence.entity.LunarStrike;
-import com.cmdpro.datanessence.registry.AttachmentTypeRegistry;
-import com.cmdpro.datanessence.registry.BlockEntityRegistry;
-import com.cmdpro.datanessence.registry.BlockRegistry;
-import com.cmdpro.datanessence.registry.EntityRegistry;
+import com.cmdpro.datanessence.registry.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -14,6 +11,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -23,7 +21,9 @@ import org.apache.commons.lang3.RandomUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class LunarCrystalSeedBlockEntity extends BlockEntity {
     public LunarCrystalSeedBlockEntity(BlockPos pos, BlockState state) {
@@ -71,6 +71,70 @@ public class LunarCrystalSeedBlockEntity extends BlockEntity {
                     }
                 }
             }
+        } else {
+            pBlockEntity.wispTimer--;
+            if (pBlockEntity.wispTimer <= 0) {
+                pBlockEntity.wispTimer = pLevel.getRandom().nextIntBetweenInclusive(5, 25);
+                int wisps = pLevel.getRandom().nextIntBetweenInclusive(1, 2);
+                for (int i = 0; i < wisps; i++) {
+                    pBlockEntity.wisps.add(LunarCrystalSeedWisp.create(pLevel.getRandom()));
+                }
+            }
+            List<LunarCrystalSeedWisp> wispsToRemove = new ArrayList<>();
+            for (LunarCrystalSeedWisp i : pBlockEntity.wisps) {
+                i.tick(pLevel, pPos.getCenter());
+                if (i.shouldDelete()) {
+                    wispsToRemove.add(i);
+                }
+            }
+            for (LunarCrystalSeedWisp i : wispsToRemove) {
+                pBlockEntity.wisps.remove(i);
+            }
+        }
+    }
+    public int wispTimer;
+    public List<LunarCrystalSeedWisp> wisps = new ArrayList<>();
+    public static class LunarCrystalSeedWisp {
+        public static final float speedMult = 10;
+        private RandomSource random;
+        public int lifetime;
+        public int initialLifetime;
+        public Vec3 offset;
+        public Vec3 motion;
+        public int timeUntilRedirect;
+        private boolean deletionQueued;
+        public void tick(Level level, Vec3 center) {
+            timeUntilRedirect--;
+            if (timeUntilRedirect <= 0) {
+                motion = new Vec3(random.nextFloat()-0.5, random.nextFloat()-0.5, random.nextFloat()-0.5).normalize().scale(speedMult);
+                timeUntilRedirect = random.nextIntBetweenInclusive(5, 10);
+            }
+            for (int i = 0; i < 5; i++) {
+                offset = offset.add(motion.scale(1f / 20f).scale(1f/5f));
+                addParticles(level, center);
+            }
+            lifetime--;
+            if (lifetime <= 0) {
+                queueDeletion();
+            }
+        }
+        public void addParticles(Level level, Vec3 center) {
+            Vec3 vec = center.add(offset);
+            level.addParticle(new CircleParticleOptionsAdditive(new Color(EssenceTypeRegistry.LUNAR_ESSENCE.get().color)), vec.x, vec.y, vec.z, 0, 0, 0);
+        }
+        public void queueDeletion() {
+            deletionQueued = true;
+        }
+        public boolean shouldDelete() {
+            return deletionQueued;
+        }
+        public static LunarCrystalSeedWisp create(RandomSource random) {
+            LunarCrystalSeedWisp wisp = new LunarCrystalSeedWisp();
+            wisp.initialLifetime = 150;
+            wisp.lifetime = wisp.initialLifetime;
+            wisp.offset = new Vec3(0, 0, 0);
+            wisp.random = random;
+            return wisp;
         }
     }
 }
