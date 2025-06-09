4923995504b70e52239048b8e5bc856afe12e1ad
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2025-06-09 09:24:41 -0400

    databank adapting

--- src/main/java/com/cmdpro/datanessence/data/hidden/EntryCondition.java
+++ src/main/java/com/cmdpro/datanessence/data/hidden/EntryCondition.java
@@ -1,7 +1,9 @@
 package com.cmdpro.datanessence.data.hidden;
 
+import com.cmdpro.databank.DatabankRegistries;
 import com.cmdpro.databank.DatabankUtils;
 import com.cmdpro.databank.hidden.HiddenCondition;
+import com.cmdpro.databank.hidden.types.BlockHiddenType;
 import com.cmdpro.datanessence.data.datatablet.Entries;
 import com.cmdpro.datanessence.data.datatablet.Entry;
 import com.cmdpro.datanessence.registry.AttachmentTypeRegistry;
@@ -9,9 +11,19 @@ import com.google.gson.JsonObject;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentSerialization;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Optional;
 
 public class EntryCondition extends HiddenCondition {
     public ResourceLocation entry;
@@ -22,6 +34,9 @@ public class EntryCondition extends HiddenCondition {
     }
     @Override
     public boolean isUnlocked(Player player) {
+        if (player == null) {
+            return true;
+        }
         if (player.getData(AttachmentTypeRegistry.UNLOCKED).contains(entry)) {
             return true;
         } else if (completionStage != -1) {
@@ -45,6 +60,19 @@ public class EntryCondition extends HiddenCondition {
         @Override
         public MapCodec<EntryCondition> codec() {
             return CODEC;
+        }
+
+        public static final StreamCodec<RegistryFriendlyByteBuf, EntryCondition> STREAM_CODEC = StreamCodec.of((buf, val) -> {
+            buf.writeResourceLocation(val.entry);
+            buf.writeInt(val.completionStage);
+        }, (buf) -> {
+            ResourceLocation entry = buf.readResourceLocation();
+            int completionStage = buf.readInt();
+            return new EntryCondition(entry, completionStage);
+        });
+        @Override
+        public StreamCodec<RegistryFriendlyByteBuf, EntryCondition> streamCodec() {
+            return STREAM_CODEC;
         }
     }
 }
