6c035dee359163e4e645c4eda512cf2b6bf4f64a
Gronglegrowth <fyredragonmagus@gmail.com>
2025-06-09 22:23:48 -0400

    meter - entry and texture are still todo

--- modchanges.md
+++ modchanges.md
@@ -1,3 +1,6 @@
+### Additions
+- Essence Meter, a tool you can use on blocks with no gui display (such as Buffers) to see how much Essence they contain
+
 ### Changes
 - Tweaked UnderTheSky music disc texture
 - Added wires minigames to the data bank entries for the Essence Leech and Industrial Plant Siphon
---
+++ src/main/java/com/cmdpro/datanessence/item/equipment/EssenceMeter.java
@@ -0,0 +1,50 @@
+package com.cmdpro.datanessence.item.equipment;
+
+import com.cmdpro.datanessence.api.essence.EssenceBlockEntity;
+import com.cmdpro.datanessence.api.essence.EssenceType;
+import com.cmdpro.datanessence.registry.EssenceTypeRegistry;
+
+import com.cmdpro.datanessence.registry.SoundRegistry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import org.jetbrains.annotations.NotNull;
+
+public class EssenceMeter extends Item {
+
+    public EssenceMeter(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(UseOnContext context) {
+        Level world = context.getLevel();
+        BlockEntity tile = world.getBlockEntity(context.getClickedPos());
+        Player player = context.getPlayer();
+
+        if ( tile instanceof EssenceBlockEntity machine ) {
+            var storage = machine.getStorage();
+            var component = Component.translatable("item.datanessence.essence_meter.contains");
+
+            for (EssenceType type : EssenceTypeRegistry.ESSENCE_TYPES.getRegistry().get()) {
+                if (storage.getEssence(type) > 0) {
+                    component.append("\n  " + storage.getEssence(type) + " / " + storage.getMaxEssence() + " ");
+                    component.append(type.getName());
+                }
+            }
+
+            if (world.isClientSide()) {
+                player.displayClientMessage(component, false);
+                player.playSound(SoundRegistry.UI_CLICK.get());
+            }
+
+            return InteractionResult.SUCCESS;
+        }
+
+        return InteractionResult.PASS;
+    }
+}
