a5418b483f5176cddee366d7bc305fcdbe4059a8
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2025-07-16 15:11:09 -0400

    colorblind option (more will be added to it if needed)

--- src/main/java/com/cmdpro/datanessence/config/DataNEssenceClientConfig.java
+++ src/main/java/com/cmdpro/datanessence/config/DataNEssenceClientConfig.java
@@ -17,30 +17,37 @@ public class DataNEssenceClientConfig {
     }
     public DataNEssenceClientConfig(ModConfigSpec.Builder builder) {
         builder.push("shaders");
-        genderEuphoriaShaderValue = buildBoolean(builder, "genderEuphoriaShader", "all", true, "Should the gender euphoria shader be enabled?");
-        pingShaderValue = buildBoolean(builder, "pingShader", "all", true, "Should the signal tracker ping shader be enabled?");
-        progressionShaderValue = buildBoolean(builder, "progressionShader", "all", true, "Should the progression shader be enabled?");
+        genderEuphoriaShaderValue = buildBoolean(builder, "genderEuphoriaShader", true, "Should the gender euphoria shader be enabled?");
+        pingShaderValue = buildBoolean(builder, "pingShader", true, "Should the signal tracker ping shader be enabled?");
+        progressionShaderValue = buildBoolean(builder, "progressionShader", true, "Should the progression shader be enabled?");
+        builder.pop();
+        builder.push("accessibility");
+        colorblindValue = buildBoolean(builder, "colorblind", false, "Should tweaks be made to hopefully help if you are colorblind?");
+        builder.pop();
     }
-    private static ModConfigSpec.BooleanValue buildBoolean(ModConfigSpec.Builder builder, String name, String category, boolean defaultValue, String comment) {
+    private static ModConfigSpec.BooleanValue buildBoolean(ModConfigSpec.Builder builder, String name, boolean defaultValue, String comment) {
         return builder.comment(comment).translation(name).define(name, defaultValue);
     }
-    private static ModConfigSpec.IntValue buildInteger(ModConfigSpec.Builder builder, String name, String category, int defaultValue, int min, int max, String comment) {
+    private static ModConfigSpec.IntValue buildInteger(ModConfigSpec.Builder builder, String name, int defaultValue, int min, int max, String comment) {
         return builder.comment(comment).translation(name).defineInRange(name, defaultValue, min, max);
     }
-    private static ModConfigSpec.DoubleValue buildDouble(ModConfigSpec.Builder builder, String name, String category, double defaultValue, double min, double max, String comment) {
+    private static ModConfigSpec.DoubleValue buildDouble(ModConfigSpec.Builder builder, String name, double defaultValue, double min, double max, String comment) {
         return builder.comment(comment).translation(name).defineInRange(name, defaultValue, min, max);
     }
     public static boolean genderEuphoriaShader = true;
     public static boolean pingShader = true;
     public static boolean progressionShader = true;
+    public static boolean colorblind = false;
     public final ModConfigSpec.BooleanValue genderEuphoriaShaderValue;
     public final ModConfigSpec.BooleanValue pingShaderValue;
     public final ModConfigSpec.BooleanValue progressionShaderValue;
+    public final ModConfigSpec.BooleanValue colorblindValue;
     public static void bake(ModConfig config) {
         try {
             genderEuphoriaShader = CLIENT.genderEuphoriaShaderValue.get();
             pingShader = CLIENT.pingShaderValue.get();
             progressionShader = CLIENT.progressionShaderValue.get();
+            colorblind = CLIENT.colorblindValue.get();
         } catch (Exception e) {
             DataNEssence.LOGGER.warn("[DATANESSENCE] Failed to load client config!");
             e.printStackTrace();
